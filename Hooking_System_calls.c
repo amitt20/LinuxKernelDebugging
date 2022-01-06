17e9db62560ddc30d5202ccdcc816b1b99b9f820
amitt20 <62395805+amitt20@users.noreply.github.com>
2022-01-06 17:24:34 +0530

    Update Hooking_System_calls.c

--- Hooking_System_calls.c
+++ Hooking_System_calls.c
@@ -5,41 +5,58 @@
 #include <asm/semaphore.h>
 #include <asm/cacheflush.h>
 
-MODULE_AUTHOR("Amit Kumar");
-MODULE_DESCRIPTION("A sample system call hooking module");
-MODULE_LICENSE("GPL");
-
 void **sys_call_table;
 
-asmlinkage int (*original_call) (const char*, int, int);
+asmlinkage int (*orig_call) (const char*, int, int);
 
-asmlinkage int our_sys_open(const char* file, int flags, int mode)
+asmlinkage int dummy_open_hook(const char* file, int flags, int mode)
 {
    printk("A file was opened\n");
    return original_call(file, flags, mode);
 }
 
-int set_page_rw(long unsigned int _addr)
+int set_page_rw(unsigned long addr)
 {
-   struct page *pg;
-   pgprot_t prot;
-   pg = virt_to_page(_addr);
-   prot.pgprot = VM_READ | VM_WRITE;
-   return change_page_attr(pg, 1, prot);
+    unsigned int level;
+    pte_t *pte = lookup_address(addr, &level);
+    if (pte->pte &~ _PAGE_RW) pte->pte |= _PAGE_RW;
+    return 0;
 }
+
+//Marking the page again readonly after doing the change
+int set_page_ro(unsigned long addr)
+{
+    unsigned int level;
+    pte_t *pte = lookup_address(addr, &level);
+    pte->pte = pte->pte &~_PAGE_RW;
+    return 0;
+}
+
 
 int init_module()
 {
     // sys_call_table address in System.map
     sys_call_table = (void*)0xc061e4e0;
-    original_call = sys_call_table[__NR_open];
+   
+   // Preserving the original call
+    orig_call = sys_call_table[__NR_open];
 
+   //Making the sys_call_table writable as by default it is readonly.
     set_page_rw(sys_call_table);
-    sys_call_table[__NR_open] = our_sys_open;
+   
+   //Assigning our stub function
+    sys_call_table[__NR_open] = dummy_open_hook;
 }
 
 void cleanup_module()
 {
    // Restore the original call
-   sys_call_table[__NR_open] = original_call;
+   sys_call_table[__NR_open] = orig_call;
+   set_page_ro(sys_call_table);
 }
+
+
+MODULE_AUTHOR("Amit Kumar");
+MODULE_DESCRIPTION("A sample system call hooking module");
+MODULE_LICENSE("GPL");
+
